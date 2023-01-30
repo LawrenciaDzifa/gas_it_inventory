7902ed9d520a00c9141cbb5cdd0cb42f406a887d
LawrenciaDzifa <dzifa01ahiadeke@gmail.com>
2023-01-30 10:30:31 +0000

    menu update

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -33,27 +33,44 @@
 </li>
 
 
-<li class="nav-item">
-    <a href="{{ route('restocks.index') }}"
-       class="nav-link {{ Request::is('restocks*') ? 'active' : '' }}">
-       <i class="fa fa-cart-plus"></i>
+<li class="nav-item dropdown">
+    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownRestocks" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fa fa-cart-plus"></i>
         <p>Restocks</p>
     </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdownRestocks">
+        <a href="{{ route('restocks.index') }}"
+           class="dropdown-item {{ Request::is('restocks*') ? 'active' : '' }}">
+            Restock List
+        </a>
+        <a href="{{ route('restocks.create') }}"
+           class="dropdown-item {{ Request::is('restocks/create') ? 'active' : '' }}">
+            Add Restock
+        </a>
+    </div>
 </li>
 
+<li class="nav-item dropdown">
+    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownRequisitions" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fa fa-shopping-basket"></i>
+        <p>Requisitions</p>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdownRequisitions">
+        <a href="{{ route('requisitions.index') }}"
+           class="dropdown-item {{ Request::is('requisitions*') ? 'active' : '' }}">
+            Requisition List
+        </a>
+        <a href="{{ route('requisitions.create') }}"
+           class="dropdown-item {{ Request::is('requisitions/create') ? 'active' : '' }}">
+            Place Request
+        </a>
+    </div>
+</li>
 <li class="nav-item">
     <a href="{{ route('stockHistories.index') }}"
        class="nav-link {{ Request::is('stockHistories*') ? 'active' : '' }}">
        <i class="fa fa-archive "></i>
         <p>Stock Histories</p>
-    </a>
-</li>
-
-<li class="nav-item">
-    <a href="{{ route('requisitions.index') }}"
-       class="nav-link {{ Request::is('requisitions*') ? 'active' : '' }}">
-       <i class="fa fa-shopping-basket"></i>
-        <p>Requisitions</p>
     </a>
 </li>
 
