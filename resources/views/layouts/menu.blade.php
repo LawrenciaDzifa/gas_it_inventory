e534f96de1de6d919467ab61d5b7a4351734c50a
LawrenciaDzifa <dzifa01ahiadeke@gmail.com>
2023-01-31 09:35:37 +0000

    permissions update

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,77 +1,80 @@
-@if(Auth::user()->role == 'admin')
-
-<li class="nav-item">
-    <a href="{{ route('items.index') }}"
-       class="nav-link {{ Request::is('items*') ? 'active' : '' }}">
-       <i class="fa fa-cubes"></i>
-        <p>Items</p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="{{ route('categories.index') }}"
-       class="nav-link {{ Request::is('categories*') ? 'active' : '' }}">
-       <i class="fa fa-cogs"></i>
-        <p>Categories</p>
-    </a>
-</li>
+@if (Auth::user()->role == 'admin')
+    <li class="nav-item">
+        <a href="{{ route('items.index') }}" class="nav-link {{ Request::is('items*') ? 'active' : '' }}">
+            <i class="fa fa-cubes"></i>
+            <p>Items</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="{{ route('categories.index') }}" class="nav-link {{ Request::is('categories*') ? 'active' : '' }}">
+            <i class="fa fa-cogs"></i>
+            <p>Categories</p>
+        </a>
+    </li>
 @endif
 <li class="nav-item dropdown">
-    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownStocks" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownStocks" role="button" data-toggle="dropdown"
+        aria-haspopup="true" aria-expanded="false">
         <i class="fa fa-database"></i>
         <p>Stocks</p>
     </a>
     <div class="dropdown-menu" aria-labelledby="navbarDropdownStocks">
-        <a href="{{ route('stocks.index') }}"
-           class="dropdown-item {{ Request::is('stocks*') ? 'active' : '' }}">
+        <a href="{{ route('stocks.index') }}" class="dropdown-item {{ Request::is('stocks*') ? 'active' : '' }}">
             Stock List
         </a>
-        <a href="{{ route('stocks.create') }}"
-           class="dropdown-item {{ Request::is('stocks/create') ? 'active' : '' }}">
-            Add Stock
-        </a>
+        @if (Auth::user()->role == 'admin')
+            <a href="{{ route('stocks.create') }}"
+                class="dropdown-item {{ Request::is('stocks/create') ? 'active' : '' }}">
+                Add Stock
+            </a>
+        @endif
+
     </div>
 </li>
+@if (Auth::user()->role =='admin')
+
 <li class="nav-item dropdown">
-    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownRestocks" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownRestocks" role="button" data-toggle="dropdown"
+        aria-haspopup="true" aria-expanded="false">
         <i class="fa fa-cart-plus"></i>
         <p>Restocks</p>
     </a>
     <div class="dropdown-menu" aria-labelledby="navbarDropdownRestocks">
-        <a href="{{ route('restocks.index') }}"
-           class="dropdown-item {{ Request::is('restocks*') ? 'active' : '' }}">
+        <a href="{{ route('restocks.index') }}" class="dropdown-item {{ Request::is('restocks*') ? 'active' : '' }}">
             Restock List
         </a>
         <a href="{{ route('restocks.create') }}"
-           class="dropdown-item {{ Request::is('restocks/create') ? 'active' : '' }}">
+            class="dropdown-item {{ Request::is('restocks/create') ? 'active' : '' }}">
             Add Restock
         </a>
     </div>
 </li>
+@endif
+
 <li class="nav-item dropdown">
-    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownRequisitions" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownRequisitions" role="button"
+        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="fa fa-shopping-basket"></i>
         <p>Requisitions</p>
     </a>
     <div class="dropdown-menu" aria-labelledby="navbarDropdownRequisitions">
         <a href="{{ route('requisitions.index') }}"
-           class="dropdown-item {{ Request::is('requisitions*') ? 'active' : '' }}">
+            class="dropdown-item {{ Request::is('requisitions*') ? 'active' : '' }}">
             Requisition List
         </a>
         <a href="{{ route('requisitions.create') }}"
-           class="dropdown-item {{ Request::is('requisitions/create') ? 'active' : '' }}">
+            class="dropdown-item {{ Request::is('requisitions/create') ? 'active' : '' }}">
             Place Request
         </a>
     </div>
 </li>
+@if (Auth::user()->role=='admin')
+
 <li class="nav-item">
     <a href="{{ route('stockHistories.index') }}"
-       class="nav-link {{ Request::is('stockHistories*') ? 'active' : '' }}">
-       <i class="fa fa-archive "></i>
+        class="nav-link {{ Request::is('stockHistories*') ? 'active' : '' }}">
+        <i class="fa fa-archive "></i>
         <p>Stock Histories</p>
     </a>
 </li>
-
-
-
-
-
+@endif
