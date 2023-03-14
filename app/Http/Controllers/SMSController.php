5972f83586141da731a0ba66a3e7b371e900ba69
LawrenciaDzifa <dzifa01ahiadeke@gmail.com>
2023-03-14 21:13:23 +0000

    refactor SMS notification

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
 class SMSController extends Controller
 {
     public function sendSMS(String $message, string $phone)
     {
-        $phone ;
+        require __DIR__ . '/vendor/autoload.php';
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+        $apiKey = getenv('SMS_API_KEY');
+        $phone;
         $message;
-
-        $url = 'https://sms.arkesel.com/sms/api?action=send-sms&api_key=Ok1GNWlYWFB0VHI1NHJZUUQ=&to=' . $phone .'&from=GASIT&sms='. '&sms=' . urlencode($message);
+        $url = 'https://sms.arkesel.com/sms/api?action=send-sms&api_key= ' . $apiKey . '&to=' . $phone . '&from=GASIT&sms=' . '&sms=' . urlencode($message);
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -22,35 +26,4 @@ class SMSController extends Controller
 
         return $output;
     }
-    // public function sendApprovalSMS(String $message, string $phone)
-    // {
-    //     $phone ;
-    //     $message;
-
-    //     $url = 'https://sms.arkesel.com/sms/api?action=send-sms&api_key=Ok1GNWlYWFB0VHI1NHJZUUQ=&to=' . $phone .'&from=GASIT&sms='. '&sms=' . urlencode($message);
-
-    //     $ch = curl_init();
-    //     curl_setopt($ch, CURLOPT_URL, $url);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //     $output = curl_exec($ch);
-    //     curl_close($ch);
-
-    //     return $output;
-    // }
-    // public function sendDeclinedSMS(String $message, string $phone)
-    // {
-    //     $phone ;
-    //     $message;
-
-    //     $url = 'https://sms.arkesel.com/sms/api?action=send-sms&api_key=Ok1GNWlYWFB0VHI1NHJZUUQ=&to=' . $phone .'&from=GASIT&sms='. '&sms=' . urlencode($message);
-
-    //     $ch = curl_init();
-    //     curl_setopt($ch, CURLOPT_URL, $url);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //     $output = curl_exec($ch);
-    //     curl_close($ch);
-
-    //     return $output;
-    // }
 }
-
