f3fe57227dd3bdf33ae5060a6722cd6cc011a425
LawrenciaDzifa <dzifa01ahiadeke@gmail.com>
2023-03-15 21:02:17 +0000

    refactor SMS notification

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -5,18 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-
 class SMSController extends Controller
 {
     public function sendSMS(String $message, string $phone)
     {
-        require __DIR__ . '/vendor/autoload.php';
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
-        $apiKey = getenv('SMS_API_KEY');
-        $phone;
+
+        $phone ;
         $message;
-        $url = 'https://sms.arkesel.com/sms/api?action=send-sms&api_key= ' . $apiKey . '&to=' . $phone . '&from=GASIT&sms=' . '&sms=' . urlencode($message);
+        $url = 'https://sms.arkesel.com/sms/api?action=send-sms&api_key=' . env('ARKESEL_API_KEY') . '&to=' . $phone .'&from=GASIT&sms='. '&sms=' . urlencode($message);
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -27,3 +23,4 @@ class SMSController extends Controller
         return $output;
     }
 }
+
